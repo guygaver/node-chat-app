4fa7d485b387d02ee7c420acd5758132270f4f69
Guy G <guy.gavergun@gmail.com>
2018-07-20 19:28:02 -0400

    Fix issue with port not passing to app.listen

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const parser = require('body-parser');
 const app = express();
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use(parser.json());
-app.use("/", express.static(publicPath));
-app.listen(3001, () => {
-    console.log(`App started on port 3000`);
+app.use(express.static(publicPath));
+app.listen(port, () => {
+    console.log(`App started on port ${port}`);
 });
