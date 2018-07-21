9ded9686e58dc9669dd3b0db37f1492da67c64bf
Guy G <guy.gavergun@gmail.com>
2018-07-21 19:15:22 -0400

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,21 +10,29 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     let formattedTime = moment(message.createdAt).format('h:mm a');
-    let li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-    jQuery('#messages').append(li);
+    let template = jQuery('#message-template').html();
+    
+    let html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (message) {
     let formattedTime = moment(message.createdAt).format('h:mm a');
-    let li = jQuery('<li></li>');
-    let a = jQuery("<a target='_blank'>My current location</a>");
+    let template = jQuery('#location-message-template').html();
+
+    let html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime,
+        url: message.url
+    });
     
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', (e) => {
@@ -50,6 +58,7 @@ $locationButton.on('click', (e) => {
     $locationButton.attr('disabled', 'disabled').text('Sending location...');
 
     navigator.geolocation.getCurrentPosition((position) => {
+        
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
